5f2d0122ce2b0400320282325eea76c1be911932
quynhanh0298 <quynhanh0298.com>
2023-05-28 15:36:10 +0700

    Fix Dashboard NavLink and Add Toggle

--- src/components/layout/SideBar/SideBar.jsx
+++ src/components/layout/SideBar/SideBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { styled } from "styled-components";
 import { Card } from "components/common/Card";
+import { NavLink } from "react-router-dom";
 import { ReactComponent as DashboardIcon } from "asserts/dashboard-icon.svg";
 import { ReactComponent as MarketIcon } from "asserts/market-icon.svg";
 import { ReactComponent as ActiveBidsIcon } from "asserts/active-bids-icon.svg";
@@ -9,8 +10,18 @@ import { ReactComponent as WalletIcon } from "asserts/wallet-icon.svg";
 import { ReactComponent as FavouritesIcon } from "asserts/favourites-icon.svg";
 import { ReactComponent as HistoryIcon } from "asserts/history-icon.svg";
 import { ReactComponent as SettingsIcon } from "asserts/setting-icon.svg";
+// import DashboardIcon from "asserts/dashboard-icon.svg";
+// import MarketIcon from "asserts/market-icon.svg";
+// import ActiveBidsIcon from "asserts/active-bids-icon.svg";
+// import MyPortfolioIcon from "asserts/my-portfolio-icon.svg";
+// import WalletIcon from "asserts/wallet-icon.svg";
+// import FavouritesIcon from "asserts/favourites-icon.svg";
+// import HistoryIcon from "asserts/history-icon.svg";
+// import SettingsIcon from "asserts/setting-icon.svg";
+
 import SideBarLogo from "asserts/sidebar-logo.svg";
 import LightModeIcon from "asserts/light-mode-icon.svg";
+import { Toggle } from "components/common/Toggle";
 
 const StyledSideBar = styled.div`
   padding: 30px;
@@ -34,14 +45,28 @@ const StyledSideBar = styled.div`
     font-size: 16px;
     color: #7a797d;
   }
+  .active {
+    svg {
+      path {
+        stroke: #5429ff;
+        fill: none;
+      }
+    }
+    .side-bar-name {
+      color: #5429ff;
+    }
+  }
 `;
-const StyledSideBarItem = styled.div`
+const StyledSideBarItem = styled(NavLink)`
   display: flex;
   flex-direction: row;
   gap: 20px;
+  text-decoration: none;
+
   svg {
     path {
       stroke: #7a797d;
+      fill: none;
     }
   }
 
@@ -52,10 +77,10 @@ const StyledSideBarItem = styled.div`
     line-height: 21px;
   }
 `;
-const SideBarItem = ({ logo, name }) => {
+const SideBarItem = ({ logo, name, to }) => {
   const Logo = logo;
   return (
-    <StyledSideBarItem>
+    <StyledSideBarItem to={to}>
       <Logo></Logo>
       <div className="side-bar-name">{name}</div>
     </StyledSideBarItem>
@@ -68,23 +93,26 @@ export const SideBar = () => {
       <div className="sidebar-logo">
         <img src={SideBarLogo} alt="" />
       </div>
-      <SideBarItem name="Dashboard" logo={DashboardIcon} />
-      <SideBarItem name="Market" logo={MarketIcon} />
-      <SideBarItem name="Active Bids" logo={ActiveBidsIcon} />
-
+      <SideBarItem to="/dashboard" name="Dashboard" logo={DashboardIcon} />
+      <SideBarItem to="/market" name="Market" logo={MarketIcon} />
+      <SideBarItem to="/active-bids" name="Active Bids" logo={ActiveBidsIcon} />
       <div className="profile">PROFILE</div>
 
-      <SideBarItem name="My Portfolio" logo={MyPortfolioIcon} />
-      <SideBarItem name="Wallet" logo={WalletIcon} />
-      <SideBarItem name="Favourites" logo={FavouritesIcon} />
-      <SideBarItem name="History" logo={HistoryIcon} />
-      <SideBarItem name="Settings" logo={SettingsIcon} />
+      <SideBarItem
+        to="/my-portfolio"
+        name="My Portfolio"
+        logo={MyPortfolioIcon}
+      />
+      <SideBarItem to="/wallet" name="Wallet" logo={WalletIcon} />
+      <SideBarItem to="/favourites" name="Favourites" logo={FavouritesIcon} />
+      <SideBarItem to="/history" name="History" logo={HistoryIcon} />
+      <SideBarItem to="/settings" name="Settings" logo={SettingsIcon} />
 
       <div className="other">OTHER</div>
       <div className="light-mode">
         <img src={LightModeIcon} alt="" />
         <div className="light-mode-text">Light Mode</div>
-        <div className="toggle"></div> {/* Code pen toggle */}
+        <Toggle zoom={0.15}></Toggle>
       </div>
 
       <Card></Card>
